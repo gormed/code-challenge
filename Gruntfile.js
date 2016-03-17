73cbe398d29d7b9868a18c6ee0b5f6eda7e6c22d
gormed <hans.ferchland@void-ptr.org>
2016-03-17 10:01:34 +0100

    * missing dev packages added, port adjusted

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9042,
+                port: 9023,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
                 livereload: 35729
